7bb864c257eae42dfb7824608aeaa84f5b346c02
Karlis <karlisstekels.dev@gmail.com>
2023-03-31 13:53:14 +0300

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by karlis.stekels on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
