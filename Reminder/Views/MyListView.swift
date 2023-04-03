e6541e137915ba5895acc8b729df5ca62793cac7
Karlis <karlisstekels.dev@gmail.com>
2023-04-03 15:39:24 +0300

    Fix issue with navigation

--- Reminder/Views/MyListView.swift
+++ Reminder/Views/MyListView.swift
@@ -12,27 +12,25 @@ struct MyListView: View {
     let myLists: FetchedResults<MyList>
     
     var body: some View {
-        NavigationView {
-            if myLists.isEmpty {
-                Spacer()
-                Text("No Reminders found")
-            } else {
-                ForEach(myLists, id: \.self) { myList in
-                    NavigationLink(value: myList) {
-                        VStack {
-                            MyListCellView(myList: myList)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding([.leading], 10)
-                                .font(.title3)
-                            Divider()
-                        }
+        if myLists.isEmpty {
+            Spacer()
+            Text("No Reminders found")
+        } else {
+            ForEach(myLists, id: \.self) { myList in
+                NavigationLink(value: myList) {
+                    VStack {
+                        MyListCellView(myList: myList)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding([.leading], 10)
+                            .font(.title3)
+                        Divider()
                     }
                 }
-                .scrollContentBackground(.hidden)
-                .navigationDestination(for: MyList.self) { myList in
-                    MyListDetailView(myList: myList)
-                        .navigationTitle(myList.name)
-                }
+            }
+            .scrollContentBackground(.hidden)
+            .navigationDestination(for: MyList.self) { myList in
+                MyListDetailView(myList: myList)
+                    .navigationTitle(myList.name)
             }
         }
     }
